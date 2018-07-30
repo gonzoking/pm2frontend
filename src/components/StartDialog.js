1282344996a45d286692b790999d3cb7c260d02e
Guy barsade <guy@zerto.com>
2018-07-30 14:57:26 +0300

    sdds

--- src/components/StartDialog.js
+++ src/components/StartDialog.js
@@ -39,14 +39,17 @@ export class StartDialog extends React.Component {
 
 
     openDialog(){
-        const files = remote.dialog.showOpenDialog({properties: ['openFile']});
+        const files = remote.dialog.showOpenDialog({properties: ['openFile','multiSelections'],filters: [{name: 'Json', extensions: ['json']}]});
         this.setState({file: files[0], startDisabled: false});
 
     }
 
     startScript(){
         let fileObject = require(this.state.file);
-        fileObject.cwd = path.join(this.state.dir ,fileObject.cwd);
+        const dir = (path.parse(this.state.file).dir).replace('\\Exec','');
+
+        fileObject.cwd = path.join(dir ,fileObject.cwd);
+
         this.setState({showBlock:true});
         pm2RunAction('start', fileObject).catch((err) => {
             console.log(err);
@@ -65,8 +68,8 @@ export class StartDialog extends React.Component {
                 {this.state.showBlock ? <div className="blockDiv">WORKING...</div> : ''}
                 <TextField hintText="Select a Json file" value={this.state.file} floatingLabelText="Script to run" floatingLabelFixed={true} style={{width:'540px'}} />
                 <FlatButton  label="..." onClick={this.openDialog} style={{fontWeight:'bold',width:'20px',minWidth:'40px'}}  />
-                <TextField value={this.state.dir} onChange={this.handleDirChange} floatingLabelText="Root directory" floatingLabelFixed={true} style={{width:'540px'}} />
-                <IconButton tooltip="Save in storage"  onClick={this.saveDir} style={{width:'25px'}}><img src="../images/disk.png" width={25} height={25} /></IconButton>
+                {/*<TextField value={this.state.dir} onChange={this.handleDirChange} floatingLabelText="Root directory" floatingLabelFixed={true} style={{width:'540px'}} />
+                <IconButton tooltip="Save in storage"  onClick={this.saveDir} style={{width:'25px'}}><img src="../images/disk.png" width={25} height={25} /></IconButton>*/}
                 {this.state.showerror ? <div className="start__error">An error occurd while starting the service!</div> : ''}
                 <FlatButton disabled={this.state.startDisabled}  label="Start" onClick={this.startScript} style={{position:'absolute',bottom:20,right:10,fontWeight:'bold',width:'20px',minWidth:'80px'}}  />
             </div>
